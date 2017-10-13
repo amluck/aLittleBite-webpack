5d671251a0a0fc7b66205bf10df1aa7ded9d5e39
awei <amluck@live.com>
2017-10-13 13:02:43 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import {cube} from './math.js';
 
 function component(){
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','webpack',' ']);
+    var element = document.createElement('pre');
+    // element.innerHTML = _.join(['Hello','webpack',' ']);
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n')
     return element;
 }
 document.body.appendChild(component());
